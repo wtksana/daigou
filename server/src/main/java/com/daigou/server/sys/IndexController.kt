5b147fed7b12891a068f2b91f025a9246219baa6
wtksana <wtksana@gmail.com>
2017-03-21 18:42:13 +0800

    添加页面

--- server/src/main/java/com/daigou/server/sys/IndexController.kt
+++ server/src/main/java/com/daigou/server/sys/IndexController.kt
@@ -1,6 +1,6 @@
-package com.daigou.server
+package com.daigou.server.sys
 
-import org.slf4j.LoggerFactory
+import com.daigou.server.BaseController
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 
@@ -8,11 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping
  * Created by wt on 2017/2/8.
  */
 @Controller
-class IndexController {
-    val Log = LoggerFactory.getLogger(javaClass)!!
+class IndexController : BaseController() {
 
     @RequestMapping("/index")
     fun index(): String {
-        return "index"
+        return "/index"
     }
 }
